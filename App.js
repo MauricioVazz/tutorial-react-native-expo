1b0862bdb21a25da27636e34a4fb8ca8eb773d96
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-08-05 15:07:29 -0300

    introducao basica do react native com expo

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box1}></View>
+      <Text style={styles.tituloPrincipal}>🚀 Testando no Browser</Text>
+      <Text style={styles.subTitulo}>React Native com estilo!</Text>
+      <Text style={styles.paragrafo}>Paragrafo</Text>
+      <Text style={styles.link}>Clique Aqui</Text>
       <StatusBar style="auto" />
+      <View style={styles.box2}></View>
     </View>
   );
 }
@@ -13,8 +18,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1e1e2f',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: '20',
+  },
+  tituloPrincipal: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    color: '#00f2ff',
+    marginVertical: '10px',
+  },
+  subTitulo: {
+    fontSize: '16px',
+    color: '#ccc',
+  },
+  paragrafo: {
+    fontSize: '14px',
+    color: '#fff',
+    marginVertical: '10px',
+  },
+  link: {
+    fontSize: '12px',
+    color: '#00f2ff',
+    textDecorationLine: 'underline',
+    marginTop: '20px',
+    cursor: 'pointer',
+    hover: {
+      color: '#00bfff',
+    },
+    marginVertical: '10px',
+  },
+  box1: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#00f2ff',
+  },
+  box2: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#442c85ff',
   },
 });
