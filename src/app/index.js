d964d3d34174542bf1ea50d9264779644ee739a2
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-11-04 16:20:03 -0300

    implementando login no app

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,6 @@
 import { View, Text, Button, StyleSheet } from 'react-native'
+import { useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useRouter } from 'expo-router'
 
 export default function Initializer() {
