b946c3bfcac903efc66517d459b59ed04ec01df8
MauricioVazz <mauricio.vaz@aluno.ifsp.edu.br>
2025-11-10 15:21:59 -0300

    tela de perfil e logout😎🥶

--- src/app/(tabs)/profile.js
+++ src/app/(tabs)/profile.js
@@ -1,22 +1,34 @@
-import { View, Text, StyleSheet, Button } from "react-native-web";
-import { useRouter } from "expo-router";
-import { AsyncStorage } from '@react-native-async-storage/async-storage'
+import { View, Text, Button, StyleSheet } from 'react-native'
+import { useRouter } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useAuthStore } from '../../stores/useAuthStore'
+import { Image } from 'expo-image'
 
 export default function Profile() {
-    const router = useRouter();
 
-    const handleLogin = async () => {
-        await AsyncStorage.removeItem('logado')
+    const router = useRouter()
+    const {profile, logout} = useAuthStore()
+
+
+    const handleLogout = async () => {
+        await AsyncStorage.removeItem('userLogged')
+        logout()
         router.replace('/login')
     }
 
     return (
         <View style={styles.container}>
-            <Text>Perfil</Text>
-            <Button title="Home" onPress={() => router.replace('/')} />
-            <Button title="Contato" onPress={() => router.push('/contact')} />
-            <Button title="Sobre" onPress={() => router.push('/about')} />
-            <Button title="Logout" onPress={handleLogin} />
+            <Image 
+                source={profile?.avatar || 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'} 
+                style={{width: 100, height: 100, borderRadius: 50, marginBottom: 20}} 
+            />
+            <Text>{profile.name}</Text>
+            <Text>{profile.email}</Text>
+            
+            <Button 
+                title='Logout'
+                onPress={handleLogout}
+            />
         </View>
     )
 }
@@ -24,7 +36,7 @@ export default function Profile() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 })
--- src/app/index.js
+++ src/app/index.js
@@ -1,36 +1,42 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, ActivityIndicator } from 'react-native'
 import { useEffect } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useRouter } from 'expo-router'
+import { useAuthStore } from '../stores/useAuthStore'
 
 export default function Initializer() {
 
     const router = useRouter()
+    const { login } = useAuthStore()
 
     useEffect(() => {
         const checkLogin = async () => {
-            const isLogged = await AsyncStorage.getItem('logado')
-            if(isLogged === 'true'){
+            const userLoggedString = await AsyncStorage.getItem('userLogged')
+            const userLogged = userLoggedString ? JSON.parse(userLoggedString) : null
+
+            if (userLogged?.token) {
+                login(userLogged)
                 router.replace('/home')
             } else {
                 router.replace('/login')
             }
         }
 
-        checkLogin()
+        setTimeout(checkLogin, 2000)
     }, [])
 
     return (
         <View style={styles.container}>
-            <Text>Inicializador</Text>
-            <Button 
+            <Text>Meu Site</Text>
+            <ActivityIndicator size="large" color="#414141ff" />
+            {/* <Button 
                 title='Login'
                 onPress={() => router.navigate('/login')}
             />
             <Button 
                 title='Home'
                 onPress={() => router.navigate('/home')}
-            />
+            /> */}
         </View>
     )
 }
